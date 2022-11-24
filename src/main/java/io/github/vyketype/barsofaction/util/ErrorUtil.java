485c0c08ea44b46b701e5c6f763b0822ca38e91d
vPrototype <65795488+vyketype@users.noreply.github.com>
2022-11-23 21:20:00 -0500

    Readability Fixes

--- src/main/java/io/github/vyketype/barsofaction/util/ErrorUtil.java
+++ src/main/java/io/github/vyketype/barsofaction/util/ErrorUtil.java
@@ -6,7 +6,16 @@ import org.bukkit.Sound;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+/**
+ * Utility class for sending error messages to players.
+ */
 public class ErrorUtil {
+    /**
+     * Sends an error message to a player and plays a sound.
+     *
+     * @param sender The player to send the message.
+     * @param message The message to send.
+     */
     public static void error(CommandSender sender, String message) {
         sender.sendMessage(BarsOfAction.NAMESPACE + ChatColor.RED + message);
         if (sender instanceof Player player)
