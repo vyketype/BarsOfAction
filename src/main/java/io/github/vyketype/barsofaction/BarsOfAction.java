f66a5981e4ce9f8da0532d316640ab88d4847e88
vPrototype <65795488+vyketype@users.noreply.github.com>
2023-02-17 19:50:10 -0500

    Version 1.3.2

--- src/main/java/io/github/vyketype/barsofaction/BarsOfAction.java
+++ src/main/java/io/github/vyketype/barsofaction/BarsOfAction.java
@@ -23,7 +23,7 @@ public class BarsOfAction extends JavaPlugin {
             ChatColor.of("#5abbba") + ChatColor.BOLD + "R" +
             ChatColor.RESET;
     public static String NAMESPACE = "" + ACTIONBAR + ChatColor.DARK_GRAY + " | " + ChatColor.GRAY + "";
-    public static String VERSION = "v1.3.2-SNAPSHOT";
+    public static String VERSION = "v1.3.2";
 
     @Getter
     private Config savedBars;
