22bb7167d9dfa3107f4e9f596fb0c2eda4f48a7f
vyketype <vyketype@gmail.com>
2023-08-21 17:25:21 -0400

    Update SaveRecentsHandler.java

--- src/main/java/io/github/vyketype/barsofaction/command/SaveRecentsHandler.java
+++ src/main/java/io/github/vyketype/barsofaction/command/SaveRecentsHandler.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@Getter
 public class SaveRecentsHandler {
-    @Getter
     private final Map<UUID, String> recents = new HashMap<>();
 }
